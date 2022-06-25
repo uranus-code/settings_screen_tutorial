fbcfa70f4b50d1b18f51ab62ed0c23784de22a30
uranuscode <uranuscod@gmail.com>
2022-06-26 00:00:09 +0430

    Upload project on Github

---
+++ android/app/src/main/kotlin/com/example/settings_screen_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.settings_screen_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:settings_screen_tutorial/widgets/settings_tile.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Settings",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 40),
+              SettingsTile(
+                color: Colors.blue,
+                icon: Ionicons.person_circle_outline,
+                title: "Account",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SettingsTile(
+                color: Colors.green,
+                icon: Ionicons.pencil_outline,
+                title: "Edit Information",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SettingsTile(
+                color: Colors.black,
+                icon: Ionicons.moon_outline,
+                title: "Theme",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SettingsTile(
+                color: Colors.purple,
+                icon: Ionicons.language_outline,
+                title: "Language",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SettingsTile(
+                color: Colors.red,
+                icon: Ionicons.log_out_outline,
+                title: "Logout",
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
